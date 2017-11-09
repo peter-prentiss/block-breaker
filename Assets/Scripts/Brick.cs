ed473b4a548c25fa067f0a6e31c7b465dd181924
Peter <peter.prentiss@gmail.com>
2017-11-09 17:45:50 -0600

    Bricks emit smoke on destruction.

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -6,6 +6,8 @@ public class Brick : MonoBehaviour {
 	public AudioClip crack;
 	public Sprite[] hitSprites;
 	public static int breakableCount = 0; 
+	public GameObject smoke;
+	
 
 	private int timesHit;
 	private LevelManager levelManager;
@@ -40,10 +42,16 @@ public class Brick : MonoBehaviour {
 		if (timesHit >= maxHits) {
 			breakableCount--;
 			levelManager.BrickDestroyed();
+			PuffSmoke();
 			Destroy(gameObject);
 		} else {
 			LoadSprites();
 		}
+	}
+
+	void PuffSmoke () {
+		GameObject smokePuff = Instantiate (smoke, transform.position, Quaternion.identity) as GameObject;
+		smokePuff.particleSystem.startColor = gameObject.GetComponent<SpriteRenderer>().color;
 	}
 
 	void LoadSprites () {
