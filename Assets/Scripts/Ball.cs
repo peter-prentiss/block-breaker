2c4e7cb0ff8adbafa02822013aff3c008a99012c
Peter <peter.prentiss@gmail.com>
2017-11-09 15:33:00 -0600

    Add random velocity change on bounce.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -24,8 +24,11 @@ public class Ball : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D (Collision2D collision) {
+		Vector2 tweak = new Vector2 (Random.Range(0f, 0.2f), Random.Range(0f, 0.2f));
+
 		if (hasStarted) {
 			audio.Play();
+			rigidbody2D.velocity += tweak;
 		}
 	}
 }
